b72115fe732cc2a9f54100b2e4a7b2f9a995696e
nicolaskjh <nicolas.koh.jh@gmail.com>
2025-03-13 23:14:11 +0800

    Fix checkstyle errors

--- src/test/java/seedu/address/model/subject/SubjectTest.java
+++ src/test/java/seedu/address/model/subject/SubjectTest.java
@@ -9,8 +9,8 @@ import static seedu.address.testutil.Assert.assertThrows;
 import org.junit.jupiter.api.Test;
 
 public class SubjectTest {
-    private final String TEST_SUBJECT_1 = "math";
-    private final String TEST_SUBJECT_2 = "chemistry";
+    private static final String TEST_SUBJECT_1 = "math";
+    private static final String TEST_SUBJECT_2 = "chemistry";
 
     @Test
     public void constructor_null_throwsNullPointerException() {
