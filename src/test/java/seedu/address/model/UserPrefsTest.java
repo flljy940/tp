e77904e2711d8dfcd9469ada9b6c7de2b8a80f01
bipplane <ryanchen@live.com.sg>
2025-03-19 15:56:24 +0800

    Add JUnit tests for code coverage

--- src/test/java/seedu/address/model/UserPrefsTest.java
+++ src/test/java/seedu/address/model/UserPrefsTest.java
@@ -1,21 +1,58 @@
 package seedu.address.model;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
 
 import org.junit.jupiter.api.Test;
+
+import seedu.address.commons.core.GuiSettings;
 
 public class UserPrefsTest {
 
     @Test
     public void setGuiSettings_nullGuiSettings_throwsNullPointerException() {
-        UserPrefs userPref = new UserPrefs();
-        assertThrows(NullPointerException.class, () -> userPref.setGuiSettings(null));
+        UserPrefs userPrefs = new UserPrefs();
+        assertThrows(NullPointerException.class, () -> userPrefs.setGuiSettings(null));
     }
 
     @Test
     public void setAddressBookFilePath_nullPath_throwsNullPointerException() {
         UserPrefs userPrefs = new UserPrefs();
         assertThrows(NullPointerException.class, () -> userPrefs.setAddressBookFilePath(null));
+    }
+
+    @Test
+    public void equals() {
+        UserPrefs userPrefs = new UserPrefs();
+        UserPrefs differentUserPrefs = new UserPrefs();
+
+        // same object -> returns true
+        assertTrue(userPrefs.equals(userPrefs));
+
+        // different type -> returns false
+        assertFalse(userPrefs.equals(5));
+
+        // null object -> returns false
+        assertFalse(userPrefs.equals(null));
+
+        // different UserPrefs -> returns false
+        differentUserPrefs.setGuiSettings(new GuiSettings(1, 1, 0, 0));
+        assertFalse(userPrefs.equals(differentUserPrefs));
+
+        // same hashcode -> returns true
+        assertEquals(userPrefs.hashCode(), userPrefs.hashCode());
+
+        // different hashcode -> returns false
+        assertNotEquals(userPrefs.hashCode(), differentUserPrefs.hashCode());
+    }
+
+    @Test
+    public void getUserPrefsFilePath_returnsCorrectPath() {
+        UserPrefs userPrefs = new UserPrefs();
+        assertEquals(java.nio.file.Paths.get("data", "addressbook.json"), userPrefs.getAddressBookFilePath());
     }
 
 }
