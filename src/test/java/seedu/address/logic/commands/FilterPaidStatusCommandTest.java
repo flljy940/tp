d14bd6204e8a5b97edf73714c27c266b0e5331be
izniy <ngyinzi1303@gmail.com>
2025-04-03 02:04:06 +0800

    Fix style error

--- src/test/java/seedu/address/logic/commands/FilterPaidStatusCommandTest.java
+++ src/test/java/seedu/address/logic/commands/FilterPaidStatusCommandTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
 import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.commands.exceptions.CommandException;
