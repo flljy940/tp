27b2c4399be8f5ce46a6d55670bba5197a5bc168
bipplane <ryanchen@live.com.sg>
2025-04-07 16:07:39 +0800

    Add text wrap and fix output deletion

--- src/main/java/seedu/address/ui/ResultDisplay.java
+++ src/main/java/seedu/address/ui/ResultDisplay.java
@@ -16,8 +16,17 @@ public class ResultDisplay extends UiPart<Region> {
     @FXML
     private TextArea resultDisplay;
 
+    /**
+     * Creates an immutable output display.
+     */
     public ResultDisplay() {
         super(FXML);
+        resultDisplay.setWrapText(true);
+        resultDisplay.addEventFilter(javafx.scene.input.KeyEvent.KEY_PRESSED, event -> {
+            if (event.isControlDown() && event.getCode() == javafx.scene.input.KeyCode.BACK_SPACE) {
+                event.consume();
+            }
+        });
     }
 
     public void setFeedbackToUser(String feedbackToUser) {
